507f807a2708e363cd5ef4ede16fcb37479cf778
hemap20 <105234527+hemap20@users.noreply.github.com>
2024-06-05 13:45:58 +0000

    hema_reciprocal added

---
+++ src/hema_reciprocal_energy.C
@@ -0,0 +1,18 @@
+//calculation of reciprocal energy
+#include "libinclude.h"
+#include "const.h"
+ //4 implementations for the energy calculation
+
+ #define BASIC 1
+
+ if defined BASIC
+    double reci_energy(int K){
+        for(int kx=-K, kx<K+1, kx++){
+            for (int ky=-K, ky<K+1, ky++){
+                for(int kz=-K, kz<K+1, kz++){
+                    
+
+                }
+            }
+        }
+    }
